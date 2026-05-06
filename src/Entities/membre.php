434e766ac9b8002a707b3c72e9dbd4d7457ee408
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-06 16:10:24 +0100

    add class Membre

---
+++ src/Entities/membre.php
@@ -0,0 +1,10 @@
+<?php
+namespace LibeCore\entities;
+class Membre extends User{
+    private $type;
+    private $borrowedBooks = [];
+public function __construct($id,$name,$email,$type){
+    parent::__construct($id,$name,$email);
+    $this->type =$type;
+}
+}
