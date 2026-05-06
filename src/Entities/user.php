f3c4387300e4ceb1ca2e50c6dbf8c0678d2cddbf
Salah-mobile <tabitsalah12@gmail.com>
2026-05-06 10:26:19 +0100

    create the class user

---
+++ src/Entities/user.php
@@ -0,0 +1,12 @@
+<?php
+class User{
+    private $name;
+    private $email;
+    function __counstructor($name,$email){
+             $this->name=$name;
+             $this->email=$email;
+    }
+    public function __toString(){
+        return $this->$name." ".$this->$email;
+    }
+}
