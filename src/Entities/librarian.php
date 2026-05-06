c586edda242879fb43536908796cead55e7ea8bd
Salah-mobile <tabitsalah12@gmail.com>
2026-05-06 10:27:30 +0100

    create the class librarian

---
+++ src/Entities/librarian.php
@@ -0,0 +1,18 @@
+<?php
+require "user.php";
+class Librarian extends User{
+     public addBook($book){
+        $books[]=$book
+     }
+     public creeMembre(){
+  
+     }
+     public displaybooks($books){
+     }
+     public deleteBook(){
+
+     }
+     public function __toString(){
+          return parent::__toString
+     }
+}
