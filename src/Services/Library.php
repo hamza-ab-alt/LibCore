12a89d200fca0702b9075b9c9a6058a8e273c74a
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-07 10:02:49 +0100

    chek the functions

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -52,5 +52,27 @@ class Library {
 
         return "Book deleted successfully";
     }
+    public function __construct($id,$name,$email,$type){
+    parent::__construct($id,$name,$email);
+    $this->type =$type;
+}
+public function getBorrowedBooks(){
+    return $this->borrowedBooks;
+}
+public function addBorrowedBook($book){
+    $this->borrowedBooks[]=$book;
+}
+public function removeBorrowedBook($isbn){
+    foreach($this->borrowedBooks as $key=>$book){
+        if($book->getIsbn()==$isbn){
+            unset($this->borrowedBooks[$key]);
+            return true;
+        }
+    }
+    return false;
+}
+public function getType(){
+    return $this->type;
+}
 
 }
