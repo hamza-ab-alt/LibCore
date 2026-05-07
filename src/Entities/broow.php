14b321e9e77eefee74ee950aae4510bb7b3e28af
hamza aballagh <hamzaaballagh941@gmail.com>
2026-05-07 10:41:23 +0100

    create the class broow

--- src/Entities/broow.php
+++ src/Entities/broow.php
@@ -1,1 +1,30 @@
 <?php
+class Borrow{
+    private $membre;
+    private $book;
+    private $borrowDate;
+    private $returnDate;
+
+    public function __construct( $membre, $book, $borrowDate, $returnDate){
+        $this->membre = $membre;
+        $this->book = $book;
+        $this->borrowDate = $borrowDate;
+        $this->returnDate = $returnDate;
+    }
+
+    public function getMembre(){
+        return $this->membre;
+    }
+
+    public function getBook(){
+        return $this->book;
+    }
+
+    public function getBorrowDate(){
+        return $this->borrowDate;
+    }
+
+    public function getReturnDate(){
+        return $this->returnDate;
+    }
+}
